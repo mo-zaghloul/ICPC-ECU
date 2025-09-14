9d1f7dbd81df67156debc0fb7d874be0883feb89
Mohamed Zaghloul <152610359+mo-zaghloul@users.noreply.github.com>
2025-09-14 13:34:17 +0300

    add templates dir

---
+++ templates/file_io.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fileIO() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+    fileIO();
+}
